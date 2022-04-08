3c1f184a7932a8530e19035c019687eeb41682e0
dandza7 <dane.m.12@gmail.com>
2022-04-08 15:14:48 +0200

    Room model

---
+++ WpfApp1/Model/Room.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public enum RoomType
+    {
+        operating,
+        meeting,
+        office,
+        storage,
+        lobby
+    }
+
+    internal class Room
+    {
+        public int Id { get; set; }
+        public string Nametag { get; set; }
+        public bool Available { get; set; }
+        public RoomType Type { get; set; }
+
+
+
+        //public Appointment[] appointment;
+
+    }
+
+
+}
