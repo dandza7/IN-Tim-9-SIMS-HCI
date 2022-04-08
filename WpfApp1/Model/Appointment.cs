347eac5acff214adf0b6d0ec8eb4adc68ad7a18d
Nikola <nikolavukic333@gmail.com>
2022-04-08 13:04:43 +0200

    Pacijent GUI prototip

---
+++ WpfApp1/Model/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    internal class Appointment
+    {
+        public int Id { get; set; }
+        public DateTime Beginning { get; set; }  
+        public DateTime Ending { get; set; }
+    }
+}
---
+++ WpfApp1/View/PatientMenu.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Interaction logic for PatientMenu.xaml
+    /// </summary>
+    public partial class PatientMenu : Window, INotifyPropertyChanged
+    {
+        internal Patient Patient { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public PatientMenu()
+        {
+            InitializeComponent();
+            this.Patient = new Patient
+            {
+                Appointments = new List<Appointment>()
+                {
+                    new Appointment
+                    {
+                        Id = 1,
+                        Beginning = DateTime.Now,
+                        Ending = DateTime.Now
+                    },
+                    new Appointment
+                    {
+                        Id = 2,
+                        Beginning = DateTime.Now,
+                        Ending = DateTime.Now
+                    },
+                    new Appointment
+                    {
+                        Id = 3,
+                        Beginning = DateTime.Now,
+                        Ending = DateTime.Now
+                    },
+                    new Appointment
+                    {
+                        Id = 4,
+                        Beginning = DateTime.Now,
+                        Ending = DateTime.Now
+                    }
+                }
+            };
+            this.DataContext = this.Patient;
+        }
+
+        private void ShowAppointmentForm(object sender, RoutedEventArgs e)
+        {
+            var s = new AppointmentForm();
+            s.Show();
+        }
+
+        private void CloseAppointments(object sender, RoutedEventArgs e) => Close();
+    }
+}
