c7b07376dafee089f73d76e8405a735e36bdfc22
Nikola <nikolavukic333@gmail.com>
2022-04-08 19:31:58 +0200

    Read Appointments

--- WpfApp1/Repository/AppointmentRepository.cs
+++ WpfApp1/Repository/AppointmentRepository.cs
@@ -10,25 +10,54 @@ namespace WpfApp1.Repository
 {
     public class AppointmentRepository
     {
-        private const string NOT_FOUND_ERROR = "Account with {0}:{1} can not be found!";
+        private const string NOT_FOUND_ERROR = "Appointment with {0}:{1} can not be found!";
         private string _path;
         private string _delimiter;
+        private readonly string _datetimeFormat;
+        private int _appointmentMaxId;
 
-        public AppointmentRepository(string path, string delimiter)
+        public AppointmentRepository(string path, string delimiter, string datetimeFormat)
         {
             _path = path;
             _delimiter = delimiter;
+            _datetimeFormat = datetimeFormat;
         }
+
+        private long GetMaxId(IEnumerable<Appointment> appointments)
+        {
+            return appointments.Count() == 0 ? 0 : appointments.Max(transaction => transaction.Id);
+        }
+
         public IEnumerable<Appointment> GetAll()
         {
             return File.ReadAllLines(_path)
                 .Select(ConvertCSVFormatToAppointment)
                 .ToList();
         }
+        public Appointment Create(Appointment appointment)
+        {
+            appointment.Id = ++_appointmentMaxId;
+            AppendLineToFile(_path, ConvertAppointmentToCSVFormat(appointment));
+            return appointment;
+        }
+
         private Appointment ConvertCSVFormatToAppointment(string appointmentCSVFormat)
         {
             var tokens = appointmentCSVFormat.Split(_delimiter.ToCharArray());
+            //Console.Write("ID pregleda: {0} Datum pocetka: {1} Datum kraja: {2}\n", int.Parse(tokens[0]), DateTime.Parse(tokens[1]), DateTime.Parse(tokens[2]));
             return new Appointment(int.Parse(tokens[0]), DateTime.Parse(tokens[1]), DateTime.Parse(tokens[2]));
+        }
+        private string ConvertAppointmentToCSVFormat(Appointment appointment)
+        {
+            return string.Join(_delimiter,
+                appointment.Id,
+                appointment.Beginning.ToString(_datetimeFormat),
+                appointment.Ending.ToString(_datetimeFormat));
+        }
+
+        private void AppendLineToFile(string path, string line)
+        {
+            File.AppendAllText(path, line + Environment.NewLine);
         }
     }
 }
--- WpfApp1/Service/AppointmentService.cs
+++ WpfApp1/Service/AppointmentService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp1.Model;
+using WpfApp1.Repository;
 
 namespace WpfApp1.Service
 {
-    public class AppointmentController
+    public class AppointmentService
     {
+        private readonly AppointmentRepository _appointmentRepo;
+
+        public AppointmentService(AppointmentRepository appointmentRepo)
+        {
+            _appointmentRepo = appointmentRepo;
+        }
+
+        internal IEnumerable<Appointment> GetAll()
+        {
+            var appointments = _appointmentRepo.GetAll();
+            return appointments;
+        }
+
+        private Appointment FindAppointmentById(IEnumerable<Appointment> appointments, long id)
+        {
+            return appointments.SingleOrDefault(appointment => appointment.Id == id);
+        }
+
+        public Appointment Create(Appointment appointment)
+        {
+
+            // save appointments
+            return _appointmentRepo.Create(appointment);
+        }
     }
 }
