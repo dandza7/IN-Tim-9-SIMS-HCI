56186a63ed3a706cc5206aa742511044d7acf9e8
Vukić Nikola <nikolavukic333@gmail.com>
2022-04-08 16:36:50 +0200

    dodan csv i appointment repo (#4)

---
+++ WpfApp1/Repository/AppointmentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Repository
+{
+    public class AppointmentRepository
+    {
+        private const string NOT_FOUND_ERROR = "Account with {0}:{1} can not be found!";
+        private string _path;
+        private string _delimiter;
+
+        public AppointmentRepository(string path, string delimiter)
+        {
+            _path = path;
+            _delimiter = delimiter;
+        }
+        public IEnumerable<Appointment> GetAll()
+        {
+            return File.ReadAllLines(_path)
+                .Select(ConvertCSVFormatToAppointment)
+                .ToList();
+        }
+        private Appointment ConvertCSVFormatToAppointment(string appointmentCSVFormat)
+        {
+            var tokens = appointmentCSVFormat.Split(_delimiter.ToCharArray());
+            return new Appointment(int.Parse(tokens[0]), DateTime.Parse(tokens[1]), DateTime.Parse(tokens[2]));
+        }
+    }
+}
