40fa2017d279e051e220f59178d5fbc300a55487
Nikola <nikolavukic333@gmail.com>
2022-04-10 12:09:02 +0200

    Create and Read Appointments 2.0

--- WpfApp1/View/PatientMenu.xaml.cs
+++ WpfApp1/View/PatientMenu.xaml.cs
@@ -15,84 +15,19 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using WpfApp1.Controller;
 using WpfApp1.Model;
+using WpfApp1.View.Dialog;
+using WpfApp1.View.Model;
 
 namespace WpfApp1.View
 {
     /// <summary>
     /// Interaction logic for PatientMenu.xaml
     /// </summary>
-    public partial class PatientMenu : Window, INotifyPropertyChanged
+    public partial class PatientMenu : Window
     {
-        internal Patient Patient { get; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private int _id;
-        private DateTime _beginning;
-        private DateTime _ending;
-        private readonly AppointmentController _appointmentController;
-
-        public int Id
-        {
-            get => _id;
-            set
-            {
-                if (_id != value)
-                {
-                    _id = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-
-        public DateTime Beginning
-        {
-            get => _beginning;
-            set
-            {
-                if (_beginning != value)
-                {
-                    _beginning = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        public DateTime Ending
-        {
-            get => _ending;
-            set
-            {
-                if (_ending != value)
-                {
-                    _ending = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
         public PatientMenu()
         {
             InitializeComponent();
-            var app = Application.Current as App;
-            _appointmentController = app.AppointmentController;
-            this.Patient = new Patient
-            {
-                Appointments = _appointmentController.GetAll().ToList()
-            };
-            this.DataContext = this.Patient;
         }
-
-        private void ShowAppointmentForm(object sender, RoutedEventArgs e)
-        {
-            var s = new AppointmentForm();
-            s.Show();
-        }
-
-        
     }
 }
