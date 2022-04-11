d9ee7ceffe9c424d624db57d902db9bf3270aeb5
Dane Milišić <83913316+dandza7@users.noreply.github.com>
2022-04-11 15:14:55 +0200

    Mcsr executive (#8)
    * List of rooms pt1
    
    * CUD added

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -20,13 +20,21 @@ namespace WpfApp1
         private static string _projectPath = System.Reflection.Assembly.GetExecutingAssembly().Location
             .Split(new string[] { "bin" }, StringSplitOptions.None)[0];
         private string APPOINTMENT_FILE = _projectPath + "\\Resources\\Data\\appointments.csv";
+        private string ROOM_FILE = _projectPath + "\\Resources\\Data\\rooms.csv";
         private const string CSV_DELIMITER = ";";
         private const string APPOINTMENT_DATETIME_FORMAT = "dd.MM.yyyy. HH:mm:ss";
 
         public AppointmentController AppointmentController { get; set; }
+        public RoomController RoomController { get; set; }
 
         public App()
         {
+            var roomRepository = new RoomRepository(ROOM_FILE, CSV_DELIMITER);
+
+            var roomService = new RoomService(roomRepository);
+
+            RoomController = new RoomController(roomService);
+
             var appointmentRepository = new AppointmentRepository(APPOINTMENT_FILE, CSV_DELIMITER, APPOINTMENT_DATETIME_FORMAT);
 
             var appointmentService = new AppointmentService(appointmentRepository);
--- WpfApp1/Model/Room.cs
+++ WpfApp1/Model/Room.cs
@@ -14,8 +14,24 @@ namespace WpfApp1.Model
         public RoomType Type { get; set; }
 
         //public Appointment[] appointment;
+        public Room(int id, string nametag, int type)
+        {
+            Id = id;
+            Nametag = nametag;
+            Available = true;
+            Type = (RoomType)type;
+        }
+        public Room(int id, string nametag, RoomType type)
+        {
+            Id = id;
+            Nametag = nametag;
+            Available = true;
+            Type = type;
+        }
 
     }
+
+    
 
     public enum RoomType
     {
