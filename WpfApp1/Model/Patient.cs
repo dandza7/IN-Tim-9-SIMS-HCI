2dd31ffee84207763d157f137b6074b787ec255b
unknown <barcaldejan@gmail.com>
2022-04-11 21:59:57 +0200

    Patient Model

--- WpfApp1/Model/Patient.cs
+++ WpfApp1/Model/Patient.cs
@@ -10,5 +10,23 @@ namespace WpfApp1.Model
     {
         public IEnumerable<Appointment> Appointments { get; set; }
 
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public Patient()
+        {
+
+        }
+
+        public Patient(int id, string name, string surname)
+        {
+            Appointments = new List<Appointment>();
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
     }
 }
