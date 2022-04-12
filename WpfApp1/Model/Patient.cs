4b19be7cbc1a64358fa046cfbbe23f7ccad8758a
dejan-1924 <83911143+dejan-1924@users.noreply.github.com>
2022-04-12 12:18:06 +0200

    Crud secretary (#11)
    * Patient Model
    
    * Secretary Patient CRUD

--- WpfApp1/Model/Patient.cs
+++ WpfApp1/Model/Patient.cs
@@ -15,6 +15,17 @@ namespace WpfApp1.Model
 
         public string Surname { get; set; }
 
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string JMBG { get; set; }
+
+
+
         public Patient()
         {
 
@@ -26,6 +37,33 @@ namespace WpfApp1.Model
             Id = id;
             Name = name;
             Surname = surname;
+        }
+
+        public Patient(string name, string surname)
+        {
+            Appointments = new List<Appointment>();
+            Name = name;
+            Surname = surname;
+        }
+
+        public Patient(int id, string name, string surname, string jMBG, string username, string password)
+        {
+            Appointments = new List<Appointment>();
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Username = username;
+            Password = password;
+            JMBG = jMBG;
+        }
+
+        public Patient(string name, string surname, string jMBG, string username, string password)
+        {
+            Name = name;
+            Surname = surname;
+            Username = username;
+            Password = password;
+            JMBG = jMBG;
         }
 
     }
