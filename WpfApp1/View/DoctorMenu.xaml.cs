b45bd88d779823c405ea4afb2f3a2d1de05186d9
Dane Milišić <83913316+dandza7@users.noreply.github.com>
2022-04-13 13:29:03 +0200

    Doctor_MyAppointmentCRUD (#18)
    Co-authored-by: madics <sergejmadic@gmail.com>

--- WpfApp1/View/DoctorMenu.xaml.cs
+++ WpfApp1/View/DoctorMenu.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Controller;
+using WpfApp1.Model;
 
 namespace WpfApp1.Service
 {
@@ -19,9 +22,102 @@ namespace WpfApp1.Service
     /// </summary>
     public partial class DoctorMenu : Window
     {
+        private AppointmentController _appointmentController;
+
+        public IEnumerable<Appointment> appointments { get; set; }
+        public List<int> Ids { get; set; }
+        public List<String> Priorities = new List<String>();
+
         public DoctorMenu()
         {
             InitializeComponent();
+            Priorities.Add("Doctor");
+            Priorities.Add("Appointment time");
+            var app = Application.Current as App;
+            _appointmentController = app.AppointmentController;
+            this.appointments = _appointmentController.GetAll();
+            this.DataContext = this;
+        }
+        private void ListButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditContainer.Visibility = Visibility.Collapsed;
+            AddContainer.Visibility = Visibility.Collapsed;
+            ListContainer.Visibility = Visibility.Visible;
+
+
+            this.appointments = _appointmentController.GetAll();
+
+        }
+
+        private void XListButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListContainer.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditContainer.Visibility = Visibility.Collapsed;
+            ListContainer.Visibility = Visibility.Collapsed;
+            AddContainer.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void XAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddContainer.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void AddConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            Appointment appointment = new Appointment(Convert.ToDateTime(StartDP.Text), Convert.ToDateTime(EndDP.Text));
+            _appointmentController.Create(appointment);
+            AddContainer.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void XEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditContainer.Visibility = Visibility.Collapsed;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditContainer.Visibility = Visibility.Visible;
+            ListContainer.Visibility = Visibility.Visible;
+            AddContainer.Visibility = Visibility.Collapsed;
+            Appointment a = ((Appointment)AppointmentGrid.SelectedItems[0]);
+            IdLabel.Content = a.Id;
+            EditStartDP.Text = a.Beginning.ToString();
+            EditEndDP.Text = a.Ending.ToString();
+        }
+
+        private void EditConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+            Appointment appointment = new Appointment(Convert.ToInt32(IdLabel.Content), Convert.ToDateTime(EditStartDP.Text), Convert.ToDateTime(EditEndDP.Text));
+            _appointmentController.Update(appointment);
+            EditContainer.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            _appointmentController.Delete(((Appointment)AppointmentGrid.SelectedItems[0]).Id);
+
+        }
+
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeleteButton.IsEnabled = true;
+            EditButton.IsEnabled = true;
+        }
+
+        private void Priority_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
