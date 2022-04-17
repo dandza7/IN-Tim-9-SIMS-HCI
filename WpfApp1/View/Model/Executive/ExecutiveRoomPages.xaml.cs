b6116c5775ad6e1f4b3e43e4443a3fe0b09dd854
Dane Milišić <83913316+dandza7@users.noreply.github.com>
2022-04-17 03:05:49 +0200

    Executive big GUI changes (#25)
    * Login Page added
    
    * MainPage added + Room Listings

---
+++ WpfApp1/View/Model/Executive/ExecutiveRoomPages.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Controller;
+using WpfApp1.Model;
+
+namespace WpfApp1.View.Model.Executive
+{
+    /// <summary>
+    /// Interaction logic for ExecutiveRoomPages.xaml
+    /// </summary>
+    public partial class ExecutiveRoomPages : Page
+    {
+        private RoomController _roomController;
+        public List<Room> Rooms { get; set; }
+        public List<String> Nametags { get; set; }
+        public List<RoomType> TypesList { get; set; }
+
+
+        public ExecutiveRoomPages()
+        {
+            InitializeComponent();
+            var app = Application.Current as App;
+            _roomController = app.RoomController;
+            this.Rooms = _roomController.GetAll();
+            var roomTypes = Enum.GetValues(typeof(RoomType));
+            TypesList = roomTypes.OfType<RoomType>().ToList();
+            this.Nametags = new List<String>();
+            foreach (Room room in Rooms)
+            {
+                Nametags.Add(room.Nametag);
+            }
+            this.DataContext = this;
+
+        }
+    }
+}
