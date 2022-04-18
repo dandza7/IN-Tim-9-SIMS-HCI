7ca433ad75152b4ea9f0e4f98220dc75974fbfec
dandza7 <dane.m.12@gmail.com>
2022-04-19 01:40:19 +0200

    MSC added and App.xaml edited

---
+++ WpfApp1/Controller/InventoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Service;
+
+namespace WpfApp1.Controller
+{
+    public class InventoryController
+    {
+        public InventoryService _inventoryService;
+
+        public InventoryController(InventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+    }
+}
---
+++ WpfApp1/Service/InventoryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Repository;
+
+namespace WpfApp1.Service
+{
+    public class InventoryService
+    {
+        public readonly InventoryRepository _inventoryRepository;
+        public readonly RoomRepository _roomRepository;
+
+        public InventoryService(InventoryRepository inventoryRepository, RoomRepository roomRepositroy)
+        {
+            _inventoryRepository = inventoryRepository;
+            _roomRepository = roomRepositroy;
+        }
+    }
+}
