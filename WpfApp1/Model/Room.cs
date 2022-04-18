df2695b8715d4c36e567e2a4e676ee7f6c1a66cb
Dane Milišić <83913316+dandza7@users.noreply.github.com>
2022-04-18 02:56:42 +0200

    Refactoring room pages (#33)
    * RRP part1
    
    * RRP part2

--- WpfApp1/Model/Room.cs
+++ WpfApp1/Model/Room.cs
@@ -11,34 +11,16 @@ namespace WpfApp1.Model
         public int Id { get; set; }
         public string Nametag { get; set; }
         public bool Available { get; set; }
-        public RoomType Type { get; set; }
+        public String Type { get; set; }
 
         //public Appointment[] appointment;
-        public Room(int id, string nametag, int type)
+        public Room(int id, string nametag, String type)
         {
             Id = id;
             Nametag = nametag;
             Available = true;
-            Type = (RoomType)type;
-        }
-        public Room(int id, string nametag, RoomType type)
-        {
-            Id = id;
-            Nametag = nametag;
-            Available = true;
-            Type = type;
+            Type =type;
         }
 
-    }
-
-    
-
-    public enum RoomType
-    {
-        operating,
-        meeting,
-        office,
-        storage,
-        lobby
     }
 }
