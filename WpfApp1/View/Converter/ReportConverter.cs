19fb3d4266ea1d93f3f838c4f40a79b452b3f17a
Nikola <nikolavukic333@gmail.com>
2022-05-07 16:00:54 +0200

    Dodao detaljan uvid u prošle appointmente

---
+++ WpfApp1/View/Converter/ReportConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.View.Model.Patient;
+
+namespace WpfApp1.View.Converter
+{
+    internal class ReportConverter : AbstractConverter
+    {
+        public static ReportDetailsView ConvertAppointmentViewAndReportToReportDetailsView(DateTime beginning, DateTime ending, string username, string nametag, string reportContent)
+        => new ReportDetailsView
+        {
+            Beginning = beginning,
+            Ending = ending,
+            Username = username,
+            Nametag = nametag,
+            ReportContent = reportContent
+        };
+    }
+}
