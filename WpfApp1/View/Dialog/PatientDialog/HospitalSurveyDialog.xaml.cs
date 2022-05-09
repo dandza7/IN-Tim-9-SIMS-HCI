4df81df670a745192a4d1d045b6915ae191912d7
Nikola <nikolavukic333@gmail.com>
2022-05-09 11:18:24 +0200

    Dodao ocjenjivanje bolnice

--- WpfApp1/View/Dialog/PatientDialog/HospitalSurveyDialog.xaml.cs
+++ WpfApp1/View/Dialog/PatientDialog/HospitalSurveyDialog.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,69 +14,37 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfApp1.Controller;
 using WpfApp1.Model;
-using WpfApp1.View.Converter;
 using WpfApp1.View.Model.Patient;
 
 namespace WpfApp1.View.Dialog.PatientDialog
 {
     /// <summary>
-    /// Interaction logic for DoctorPollDialog.xaml
+    /// Interaction logic for HospitalSurveyDialog.xaml
     /// </summary>
-    public partial class PatientSurveyDialog : Page
+    public partial class HospitalSurveyDialog : Page
     {
         private SurveyController _surveyController;
-        private AppointmentController _appointmentController;
-        private DoctorController _doctorController;
-        private RoomController _roomController;
-        private UserController _userController;
 
         private List<string> _questions;
-        private AppointmentView _appointmentView;
 
         public List<string> Questions
         {
             get { return _questions; }
             set
             {
-                if(value != null) _questions = value;
+                if (value != null) _questions = value;
             }
         }
 
-        public AppointmentView AppointmentView
-        {
-            get { return _appointmentView; }
-            set { _appointmentView = value; }
-        }
-
-        public PatientSurveyDialog()
+        public HospitalSurveyDialog()
         {
             InitializeComponent();
             DataContext = this;
 
             var app = Application.Current as App;
             _surveyController = app.SurveyController;
-            _appointmentController = app.AppointmentController;
-            _doctorController = app.DoctorController;
-            _roomController = app.RoomController;
-            _userController = app.UserController;
 
-            int appointmentId = (int)app.Properties["appointmentId"];
-            int patientId = (int)app.Properties["userId"];
-
-            Appointment appointment = _appointmentController.GetById(appointmentId);
-            User doctorUser = _userController.GetById(appointment.DoctorId);
-            Doctor doctor = _doctorController.GetById(appointment.DoctorId);
-            Room room = _roomController.GetById(doctor.RoomId);
-
-            AppointmentView appointmentView = AppointmentConverter.ConvertAppointmentAndDoctorToAppointmentView(appointment, doctorUser, room);
-            AppointmentView = appointmentView;
-            if(appointmentId != -1)
-            {
-                Questions = GetDoctorSurvey();
-            } else
-            {
-                Questions = GetHospitalSurvey();
-            }
+            Questions = GetHospitalSurvey();
         }
 
         private void GradeButton_Click(object sender, RoutedEventArgs e)
@@ -91,26 +58,14 @@ namespace WpfApp1.View.Dialog.PatientDialog
 
             var app = Application.Current as App;
             _surveyController = app.SurveyController;
-            _appointmentController = app.AppointmentController;
-            _doctorController = app.DoctorController;
 
-            int appointmentId = (int)app.Properties["appointmentId"];
             int patientId = (int)app.Properties["userId"];
-
-            if(appointmentId != -1)
-            {
-                Appointment appointment = _appointmentController.GetById(appointmentId);
-                Doctor doctor = _doctorController.GetById(appointment.DoctorId);
-                Survey completedSurvey = new Survey(patientId, doctor.Id, appointmentId, grades);
-                _surveyController.Create(completedSurvey);
-                PatientErrorMessageBox.Show("Thank you for completing the survey!");
-                NavigationService.GoBack();
-            } else {
-                Survey completedSurvey = new Survey(patientId, -1, -1, grades);
-                _surveyController.Create(completedSurvey);
-                PatientErrorMessageBox.Show("Thank you for completing the survey!");
-                NavigationService.GoBack();
-            }
+            
+            Survey completedSurvey = new Survey(patientId, -1, -1, grades);
+            _surveyController.Create(completedSurvey);
+            PatientErrorMessageBox.Show("Thank you for completing the survey!");
+            Frame patientFrame = (Frame)app.Properties["PatientFrame"];
+            patientFrame.Content = new PatientProfileView();
         }
 
         private void GoBack_Click(object sender, RoutedEventArgs e)
@@ -149,30 +104,12 @@ namespace WpfApp1.View.Dialog.PatientDialog
             Console.WriteLine(patientMenu.GetType());
             foreach (RadioButton rb in FindVisualChilds<RadioButton>(patientMenu))
             {
-                if(rb.IsChecked == true)
+                if (rb.IsChecked == true)
                 {
                     grades.Add(int.Parse(rb.Content.ToString()));
                 }
             }
             return grades;
-        }
-
-        private List<string> GetDoctorSurvey()
-        {
-            const string QUESTION_ONE = "1. How would you are interpersonal abilities of this doctor?";
-            const string QUESTION_TWO = "2. How would you are professionalism of this doctor?";
-            const string QUESTION_THREE = "3. How would you rate doctor's availability?";
-            const string QUESTION_FOUR = "4. How satisfied are you with the treatment you have received?";
-            const string QUESTION_FIVE = "5. How likely are you to come back to the same doctor if needed?";
-
-            List<string> survey = new List<string>();
-            survey.Add(QUESTION_ONE);
-            survey.Add(QUESTION_TWO);
-            survey.Add(QUESTION_THREE);
-            survey.Add(QUESTION_FOUR);
-            survey.Add(QUESTION_FIVE);
-
-            return survey;
         }
 
         private List<string> GetHospitalSurvey()
