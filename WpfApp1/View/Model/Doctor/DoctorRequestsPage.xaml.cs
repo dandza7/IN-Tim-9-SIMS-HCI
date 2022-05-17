e166ff820ba9db81f3ac7d6e49c07c47e2721d4d
madics <sergejmadic@gmail.com>
2022-05-17 11:53:04 +0200

    Doctor_Refactoring_2

--- WpfApp1/View/Model/Doctor/DoctorRequestsPage.xaml.cs
+++ WpfApp1/View/Model/Doctor/DoctorRequestsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Controller;
+using WpfApp1.Model;
 
 namespace WpfApp1.View.Model.Doctor
 {
@@ -20,9 +23,39 @@ namespace WpfApp1.View.Model.Doctor
     /// </summary>
     public partial class DoctorRequestsPage : Page
     {
-        public DoctorRequestsPage()
-        {
+        RequestController _requestController;
+        public ObservableCollection<Request> Requests;
+
+    public DoctorRequestsPage()
+    {
             InitializeComponent();
+            var app = Application.Current as App;
+            _requestController = app.RequestController;
+            Requests = new ObservableCollection<Request>();
+            Requests = (ObservableCollection<Request>)_requestController.GetAllByDoctorId(1);//DODATI ID ULOGOVANOG
+            RequestViewGrid.ItemsSource = Requests;
+            this.DataContext = this;
+        }
+
+        private void SaveBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (_requestController.Create(
+                new Request(
+                    Convert.ToDateTime(BeginningDTP.Text),
+                    Convert.ToDateTime(EndingDTP.Text),
+                    Request.RequestStatusType.Pending,
+                    1,//DODATI ID ULOGOVANOG
+                    TitleTB.Text,
+                    ContentTB.Text
+                    )) == null) exceptionLabel.Visibility = Visibility.Visible;
+            else exceptionLabel.Visibility = Visibility.Hidden;
+
+            BeginningDTP.Text="";
+            EndingDTP.Text = "";
+            TitleTB.Clear();
+            ContentTB.Clear();
+
+
         }
     }
 }
