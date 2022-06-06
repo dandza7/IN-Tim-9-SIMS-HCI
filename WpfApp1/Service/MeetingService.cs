11f1a52dd3907b763b7b265d5a9eaa0090008638
dejan-1924 <83911143+dejan-1924@users.noreply.github.com>
2022-06-06 14:49:30 +0200

    dodat pregled sastanaka (#192)

---
+++ WpfApp1/Service/MeetingService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+using WpfApp1.Repository;
+
+namespace WpfApp1.Service
+{
+    public class MeetingService
+    {
+        private readonly MeetingRepository _meetingRepo;
+
+        public MeetingService(MeetingRepository meetingRepo)
+        {
+            _meetingRepo = meetingRepo;
+        }
+        public IEnumerable<Meeting> GetAll()
+        {
+            return _meetingRepo.GetAll();
+        }
+
+        public Meeting Create(Meeting meeting)
+        {
+            return _meetingRepo.Create(meeting);
+        }
+    }
+}
